e367361d9c0306926d12344b62f2942979981f7a
JD <>
2020-06-02 19:35:40 -0600

    DAOherramienta

--- PHP/DAOherramienta.php
+++ PHP/DAOherramienta.php
@@ -1,6 +1,10 @@
 <?php
-include("PHP/credenciales.php");
-include("PHP/herramienta.php");
+include("credenciales.php");
+include("herramienta.php");
+include("categoria.php");
+include("status_uso.php");
+include("status_prestamo.php");
+include("condicion.php");
 
 class DAOherramienta{
     private $con;
@@ -9,15 +13,26 @@ class DAOherramienta{
 
     }
     public function conectar(){
+        try{
         $this->con= new mysqli(SERVIDOR, USUARIO, CONTRA, DB);
+        }catch (Exception $exc){
+            echo $exc->getTraceAsString();
+        }
     }
     public function desconectar(){
         $this->con->close();
     }
-    public function insertar($obj){
+    public function insertar($herr,$cat,$uso,$prest,$cond){
         $herr= new herramienta();
-        $herr = $obj;
-        $sql="insert into herramienta values("..", "..", "..", "..", "..", 1, 1)";
+        $cat = new categoria();
+        $uso = new status_uso();
+        $prest = new status_prestamo();
+        $cond = new condicion();        
+        $sql="insert into herramienta values('".$herr->getFecha_ingreso()."','".$herr->getNombre_herramienta()."',".$cat->getId_categoria().",".$uso->getId_status_uso().",1,1)";
+        $this->conectar();
+        if ($this->con->query($sql)){
+            
+        }
     }
 
 }
