a678eafb8f42224ce882202ce0a7b6e96926e029
JDMartinez <you@example.com>
2020-06-02 19:53:04 -0600

    DAOherramienta

--- PHP/DAOherramienta.php
+++ PHP/DAOherramienta.php
@@ -31,8 +31,47 @@ class DAOherramienta{
         $sql="insert into herramienta values('".$herr->getFecha_ingreso()."','".$herr->getNombre_herramienta()."',".$cat->getId_categoria().",".$uso->getId_status_uso().",1,1)";
         $this->conectar();
         if ($this->con->query($sql)){
-            
+            echo "<script>swal({title: 'Exito',text: 'El registro fue exitoso',icon: 'success'})</script>";
+        }else{
+            echo "<script>swal({title: 'Error',text: 'Algo salio mal, el registro no se hizo',icon: 'error'})</script>";
         }
+        $this->desconectar();
+    }
+
+    public function eliminar($codigo_herramienta){        
+        $sql="delete from herramienta where codigo_herramienta=$codigo_herramienta";
+        $this->conectar();
+        if ($this->con->query($sql)){
+            echo "<script>swal({title: 'Exito',text: 'El registro fue eliminado',icon: 'success'})</script>";
+        }else{
+            echo "<script>swal({title: 'Error',text: 'Algo salio mal, el registro no se elimino',icon: 'error'})</script>";
+        }
+        $this->desconectar();
+    }
+
+    public function cambiar($herr,$cat,$uso,$prest,$cond,$codigo_herramienta){
+        $herr= new herramienta();
+        $cat = new categoria();
+        $uso = new status_uso();
+        $prest = new status_prestamo();
+        $cond = new condicion();                
+        $sql = 
+        "UPDATE platillos SET         
+        fecha_ingreso = '".$herr->getFecha_ingreso()."',
+        nombre_herramienta = '".$herr->getNombre_herramienta()."',
+        id_categoria = .$cat->getId_categoria().,
+        id_status_uso = .$uso->getId_status_uso().,
+        id_status_prestamo = .$camb->getPrecio().,
+        id_condicion = .$camb->getPrecio().,
+        WHERE codigo = ".$codigo_herramienta.";";
+        
+        $this->conectar();
+        if ($this->con->query($sql)){
+            echo "<script>swal({title:'Exito',text:'El registro fue modificado exitosamente',icon:'success'})</script>";
+        }else{
+            echo "<script>swal({title:'Error',text:'El registro no fue modificado',icon:'error'})</script>";
+        }
+        $this->desconectar();        
     }
 
 }
