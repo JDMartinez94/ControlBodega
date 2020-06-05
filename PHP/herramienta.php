5c6a583fcd2c9726b1319430106e1292a220ed93
JD <DELL@Laptop>
2020-06-04 22:10:57 -0600

    Agregue funciones

--- PHP/herramienta.php
+++ PHP/herramienta.php
@@ -10,6 +10,14 @@
         private $codigo_herramienta; 
         private $fecha_ingreso; 
         private $nombre_herramienta; 
+        private $id_categoria;
+        private $nombre_categoria; 
+        private $id_status_uso;
+        private $status_uso;
+        private $id_status_prestamo;
+        private $status_prestamo; 
+        private $id_condicion; 
+        private $condicion; 
         
         public function __construct() {
             ;
@@ -39,7 +47,68 @@
             $this->nombre_herramienta = $nombre_herramienta;
         }
 
+        function getId_categoria() {
+            return $this->id_categoria;
+        }
 
+        function getNombre_categoria() {
+            return $this->nombre_categoria;
+        }
+
+        function setId_categoria($id_categoria) {
+            $this->id_categoria = $id_categoria;
+        }
+
+        function setNombre_categoria($nombre_categoria) {
+            $this->nombre_categoria = $nombre_categoria;
+        }
+
+        function getId_status_uso() {
+            return $this->id_status_uso;
+        }
+
+        function getStatus_uso() {
+            return $this->status_uso;
+        }
+
+        function setId_status_uso($id_status_uso) {
+            $this->id_status_uso = $id_status_uso;
+        }
+
+        function setStatus_uso($status_uso) {
+            $this->status_uso = $status_uso;
+        }
+        
+        function getId_status_prestamo() {
+            return $this->id_status_prestamo;
+        }
+
+        function getStatus_prestamo() {
+            return $this->status_prestamo;
+        }
+
+        function setId_status_prestamo($id_status_prestamo) {
+            $this->id_status_prestamo = $id_status_prestamo;
+        }
+
+        function setStatus_prestamo($status_prestamo) {
+            $this->status_prestamo = $status_prestamo;
+        }
+        function getId_condicion() {
+            return $this->id_condicion;
+        }
+
+        function getCondicion() {
+            return $this->condicion;
+        }
+
+        function setId_condicion($id_condicion) {
+            $this->id_condicion = $id_condicion;
+        }
+
+        function setCondicion($condicion) {
+            $this->condicion = $condicion;
+        }
     }
     
 
