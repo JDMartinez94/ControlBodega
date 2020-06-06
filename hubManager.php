ce108e29b09f138e39987468cc9f446f2bd43a4b
andreacastle1988 <andreacastle198@gmail.com>
2020-06-05 21:27:24 -0600

    Version10

--- hubManager.php
+++ hubManager.php
@@ -91,11 +91,7 @@ session_start();
 		</div>
 
 		<?php   
-<<<<<<< HEAD
-            //Validación de ingreso correcto take 100000
-=======
-                        //Validación de ingreso correcto take 9+
->>>>>>> 1160a79a98d61a37bd02d250586b05002e1a8021
+			//Version10
 			if($_SESSION["user"]["id_rol"]==1)
 			{
 				$username = $_SESSION["user"]["nombre"];
