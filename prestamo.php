a5e093ef1e2de3974dbd84e6d79a93eaa2999c1b
Alcides Medina <alcides.medina19@itca.edu.sv>
2020-06-07 20:25:30 -0600

    Agregando Zona Horaria Correcta

--- prestamo.php
+++ prestamo.php
@@ -5,6 +5,18 @@ include("PHP/formulas.php");
 include("PHP/DAOregistro.php");
 $dao = new DAOregistro();
 $prestamo = new registro();
+
+date_default_timezone_set('America/El_Salvador');
+
+$script_tz = date_default_timezone_get();
+
+if (strcmp($script_tz, ini_get('date.timezone'))){
+    echo 'La zona horaria del script difiere de la zona horaria de la configuracion ini.';
+} else {
+    echo 'La zona horaria del script y la zona horaria de la configuración ini coinciden.';
+}
+
+
 ?>
 
 <!DOCTYPE html>
